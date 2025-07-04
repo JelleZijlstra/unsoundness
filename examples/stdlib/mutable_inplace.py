aaf40c3bc18156b4c991dd396bfe2604ebb848ef
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-07-05 00:13:28 +0100

    Fix typo in stdlib/mutable_inplace

--- examples/stdlib/mutable_inplace.py
+++ examples/stdlib/mutable_inplace.py
@@ -8,13 +8,13 @@ is mutated in-place. In other words, `x += y` and `x = x + y` are not the same:
 xs = [1, 2, 3]
 ys = xs
 ys += [4]
-assert x == [1, 2, 3 ,4]
+assert xs == [1, 2, 3 ,4]
 
 # whereas:
 xs = [1, 2, 3]
 ys = xs
 ys = ys + [4]
-assert x == [1, 2, 3]
+assert xs == [1, 2, 3]
 ```
 """
 
