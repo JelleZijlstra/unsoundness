a86a32b34ab78761972e45f279ee8924fe485105
Lon Farr <lonfarr@gmail.com>
2025-07-04 12:22:17 -0700

    Add example for getattr returning Any

---
+++ examples/stdlib/getattr.py
@@ -0,0 +1,15 @@
+"""
+This example shows that getattr returns an `Any` type allowing for unsoundness.
+"""
+
+
+class StoreX:
+    def __init__(self, x: int) -> None:
+        self.x: str = str(x)
+
+
+def func(x: int) -> str:
+    store_x: StoreX = StoreX(x)
+    str_x: str = getattr(store_x, "x")
+
+    return str_x
