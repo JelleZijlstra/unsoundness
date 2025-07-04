7fc3aece46810e34815fef689a649dcc55a3fd82
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2025-07-03 20:31:00 -0700

    fix repeated calls

--- examples/generic/mutable_default.py
+++ examples/generic/mutable_default.py
@@ -2,11 +2,14 @@
 
 Based on an example by Anders Kaseorg in https://github.com/python/mypy/issues/13054
 """
+
+
 def f[T](x: T, y: list[T] = []) -> list[T]:
     return y
+
 
 def func(x: int) -> str:
     y_strs = f("a")
     y_ints = f(4)
     y_ints.append(x)
-    return y_strs[0]
+    return y_strs[-1]
