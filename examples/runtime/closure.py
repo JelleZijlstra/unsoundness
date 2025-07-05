d52899a0c4f1e2a593a3b8e0037f78f0d420cdea
Lon Farr <lonfarr@gmail.com>
2025-07-04 19:22:10 -0700

    Add example of how modifying __closure__ is unsound

---
+++ examples/runtime/closure.py
@@ -0,0 +1,34 @@
+"""
+If a variable is captured with closure, it is stored in the dunder
+__closure__ cell. It is possible to modify the cell contents manually,
+thus modifying the underlying value.
+
+This is not typically feasible without knowing the code, so this is
+relatively limited in how it could be used in the real world.
+"""
+
+from types import CellType
+from typing import Callable
+
+
+class ClosureTestClass:
+    def __init__(self, x: int) -> None:
+        str_x: str = str(x)
+
+        def get_str_x() -> str:
+            nonlocal str_x  # Not strictly needed but helps with clarity
+            return str_x
+
+        self.get_str_x: Callable[[], str] = get_str_x
+
+
+def func(x: int) -> str:
+    closure_test_class: ClosureTestClass = ClosureTestClass(x)
+
+    # Since this can be None or have no items, save off and run
+    # within an if statement
+    closure: tuple[CellType, ...] | None = closure_test_class.get_str_x.__closure__
+    if closure:
+        closure[0].cell_contents = x
+
+    return closure_test_class.get_str_x()
