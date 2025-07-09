73c88ea6dc0474043444d90f431a6a4880393580
Sander Van Balen <git@sandervanbalen.be>
2025-07-09 10:01:02 +0200

    updated description

--- examples/stdlib/mutable_inplace.py
+++ examples/stdlib/mutable_inplace.py
@@ -2,7 +2,9 @@
 This example is concerned with the difficulty of typing binary operators.
 
 When augmented assignment is performed on a mutable collection, the collection
-is mutated in-place. In other words, `x += y` and `x = x + y` are not the same:
+is mutated in-place. In other words, `x += y` and `x = x + y` are not necessarily the same.
+It depends on the specific implementation of the runtime type (whether it implements
+__iadd__ or not). Therefore it is not safe to make assumptions on the abstract level:
 
 ```py
 xs = [1, 2, 3]
