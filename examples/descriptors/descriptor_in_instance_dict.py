609da5d7b2f9b178fcea8432f36a61018352eefa
alwaysmpe <marc@mlwhat.com>
2025-07-11 03:59:12 +0100

    Descriptor instance/class unsoundness (#23)
    As mentioned in the discuss thread, not sure how much this counts as a
    type checker bug or a spec unsoundness problem.

---
+++ examples/descriptors/descriptor_in_instance_dict.py
@@ -0,0 +1,20 @@
+from typing import Callable
+
+
+class Desc:
+    def __call__[T](self, arg: T) -> T:
+        return arg
+
+    def __get__(self, instance: object, owner: object) -> Callable[[object], str]:
+        return str
+
+
+class C:
+    desc: Desc
+
+    def __init__(self):
+        self.desc = Desc()
+
+
+def func(x: int) -> str:
+    return C().desc(x)
