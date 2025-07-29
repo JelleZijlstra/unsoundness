3c2500f7688d61f8919e98ac1a057f2c2698dd7d
Vizonex <VizonexBusiness@gmail.com>
2025-07-29 17:17:59 -0500

    add hybrid-wrapper/descriptor example

---
+++ examples/descriptors/hybrid_descriptor_or_wrapper.py
@@ -0,0 +1,23 @@
+from typing import Callable, ParamSpec, TypeVar, Generic, Concatenate
+
+_I = TypeVar("_I")
+_T = TypeVar("_T")
+_P = ParamSpec("_P")
+
+class WrapperOrDesc(Generic[_T, _P]):
+    def __init__(self, func:Callable[_P, _T]):
+        self.func = func
+    def __call__(self, *args:_P.args, **kwds:_P.kwargs) -> _T:...
+    def __get__(self: "WrapperOrDesc[Concatenate[_I, _P], _T]", instance:_I) -> "WrapperOrDesc[_P, _T]":...
+
+
+class Class(object):
+    @WrapperOrDesc
+    def method(self, i:int) -> int:...
+
+@WrapperOrDesc
+def func(i:int) -> int:...
+
+func(1)
+Class().method(1)
+
