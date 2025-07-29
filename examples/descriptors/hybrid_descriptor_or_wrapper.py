044e61ff3bfd69e45db2329cab8299f10e80b6d6
Vizonex <VizonexBusiness@gmail.com>
2025-07-29 17:34:30 -0500

    ensure it runs

--- examples/descriptors/hybrid_descriptor_or_wrapper.py
+++ examples/descriptors/hybrid_descriptor_or_wrapper.py
@@ -5,19 +5,27 @@ _T = TypeVar("_T")
 _P = ParamSpec("_P")
 
 class WrapperOrDesc(Generic[_T, _P]):
-    def __init__(self, func:Callable[_P, _T]):
+    def __init__(self, func:Callable[_P, _T], i:object | None = None):
         self.func = func
-    def __call__(self, *args:_P.args, **kwds:_P.kwargs) -> _T:...
-    def __get__(self: "WrapperOrDesc[Concatenate[_I, _P], _T]", instance:_I, owner:type[_T]) -> "WrapperOrDesc[_P, _T]":...
+        self.i = i
+    def __call__(self, *args:_P.args, **kwds:_P.kwargs) -> _T:
+        if self.i:
+            return self.func(self.i, *args, **kwds)
+        return self.func(*args, **kwds)
+    def __get__(self: "WrapperOrDesc[Concatenate[_I, _P], _T]", instance:_I, owner:type[_T]) -> "WrapperOrDesc[_P, _T]":
+        return WrapperOrDesc(self.func, instance)
 
 
 class Class(object):
     @WrapperOrDesc
-    def method(self, i:int) -> int:...
+    def method(self, i:int) -> int:
+        return i
 
 @WrapperOrDesc
-def func(i:int) -> int:...
+def func(i:int) -> int:
+    return i
 
 func(1)
-Class().method(1)
+c = Class()
+c.method(1)
 
