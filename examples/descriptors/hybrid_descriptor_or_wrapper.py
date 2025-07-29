adb479c093aee32dfe1cc8874c1bcf6639ba1805
Vizonex <VizonexBusiness@gmail.com>
2025-07-29 17:36:54 -0500

    make it more 3.13-like

--- examples/descriptors/hybrid_descriptor_or_wrapper.py
+++ examples/descriptors/hybrid_descriptor_or_wrapper.py
@@ -1,18 +1,17 @@
-from typing import Callable, ParamSpec, TypeVar, Generic, Concatenate
+from typing import Callable, Concatenate
 
-_I = TypeVar("_I")
-_T = TypeVar("_T")
-_P = ParamSpec("_P")
 
-class WrapperOrDesc(Generic[_T, _P]):
-    def __init__(self, func:Callable[_P, _T], i:object | None = None):
+class WrapperOrDesc[**_P, _T]:
+    def __init__[**_P, _T](self, func:Callable[_P, _T], i:object | None = None):
         self.func = func
         self.i = i
+    
     def __call__(self, *args:_P.args, **kwds:_P.kwargs) -> _T:
         if self.i:
             return self.func(self.i, *args, **kwds)
         return self.func(*args, **kwds)
-    def __get__(self: "WrapperOrDesc[Concatenate[_I, _P], _T]", instance:_I, owner:type[_T]) -> "WrapperOrDesc[_P, _T]":
+    
+    def __get__[_I](self: "WrapperOrDesc[Concatenate[_I, _P], _T]", instance:_I, owner:type[_T]) -> "WrapperOrDesc[_P, _T]":
         return WrapperOrDesc(self.func, instance)
 
 
