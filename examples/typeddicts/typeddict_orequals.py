37a9345b059a7b551db979db403d84f7a5db4778
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2025-08-17 18:57:57 -0700

    format

--- examples/typeddicts/typeddict_orequals.py
+++ examples/typeddicts/typeddict_orequals.py
@@ -6,14 +6,18 @@ from typing import TypedDict
 class Base(TypedDict):
     x: int
 
+
 class Child1(Base):
     y: int
+
 
 class Child2(Base):
     y: str
 
+
 def do_it(td1: Base, td2: Base) -> None:
     td1 |= td2
+
 
 def func(x: int) -> str:
     c2: Child2 = {"x": x, "y": str(x)}
--- examples/typeddicts/typeddict_update.py
+++ examples/typeddicts/typeddict_update.py
@@ -13,14 +13,18 @@ from typing import TypedDict
 class Base(TypedDict):
     x: int
 
+
 class Child1(Base):
     y: int
+
 
 class Child2(Base):
     y: str
 
+
 def do_it(td1: Base, td2: Base) -> None:
     td1.update(td2)
+
 
 def func(x: int) -> str:
     c2: Child2 = {"x": x, "y": str(x)}
