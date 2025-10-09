948a5306b7d71ee6df026200fe1db5393cd1afed
wyattscarpenter <wyattscarpenter@gmail.com>
2025-10-08 18:54:08 -0700

    Ellipsis in Callable

---
+++ examples/callable_ellipsis.py
@@ -0,0 +1,8 @@
+from typing import Callable
+
+# Ellipsis in Callable is "gradual form" https://typing.python.org/en/latest/spec/glossary.html#term-gradual-form, so they'll just let you put any old thing in there. https://typing.python.org/en/latest/spec/callables.html#meaning-of-in-callable
+
+f: Callable[..., str] = lambda x: x
+
+def func(x: int) -> str:
+    return f(x)
