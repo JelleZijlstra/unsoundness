a08c774bfc4027c7c838ce291143f4ca559369b9
wyattscarpenter <wyattscarpenter@gmail.com>
2025-10-09 16:54:28 -0700

    lambda unannotated (#36)

---
+++ nonexamples/lambda_unannotated.py
@@ -0,0 +1,10 @@
+ACCEPTED_BY = {"mypy": True, "pyright": True}
+
+# Neither mypy nor pyright elect to infer more precise types for the missing annotations for this lambda, so they default to Any-style unsound semantics.
+# This is allowed by https://typing.python.org/en/latest/spec/annotations.html#the-meaning-of-annotations, but also it "is the result of a deliberate choice by that type checker", for both typecheckers, which is our criterion for a "nonexample".
+
+f = lambda x: x
+
+
+def func(x: int) -> str:
+    return f(x)
