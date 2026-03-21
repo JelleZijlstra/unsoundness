f2a42773cc3b4cccf9cff3634615f62d45de021b
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2026-03-21 09:23:11 -0700

    format

--- examples/attributes/delattr.py
+++ examples/attributes/delattr.py
@@ -3,13 +3,15 @@ Type checkers are generally happy to let users del attributes. However,
 this can lead to unsound behavior.
 """
 
+
 class HasGetAttr:
     def __init__(self, i: int):
         self.i = i
         self.s = str(i)
-       
+
     def __getattr__(self, attr: str) -> int:
         return self.i
+
 
 def func(i: int) -> str:
     hga = HasGetAttr(i)
