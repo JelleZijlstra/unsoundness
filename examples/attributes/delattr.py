a1a963b055e06656744e68cf0a924b7b65f24a58
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2026-03-21 09:19:34 -0700

    Add example of using del on an attribute

---
+++ examples/attributes/delattr.py
@@ -0,0 +1,17 @@
+"""
+Type checkers are generally happy to let users del attributes. However,
+this can lead to unsound behavior.
+"""
+
+class HasGetAttr:
+    def __init__(self, i: int):
+        self.i = i
+        self.s = str(i)
+       
+    def __getattr__(self, attr: str) -> int:
+        return self.i
+
+def func(i: int) -> str:
+    hga = HasGetAttr(i)
+    del hga.s
+    return hga.s
