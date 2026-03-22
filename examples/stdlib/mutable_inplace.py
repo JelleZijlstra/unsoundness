bcea0a6cbe0bebe51d050e5e174dc70ae9be9e97
Sander Van Balen <git@sandervanbalen.be>
2026-03-22 13:41:30 +0100

    refined descriptions

--- examples/stdlib/mutable_inplace.py
+++ examples/stdlib/mutable_inplace.py
@@ -53,7 +53,7 @@ We'll start with the facts, without getting into motivation or consequences:
 
 # Analysis
 
-Before we dive into the specifics of these six facts, let's look at how all this plays together to result in the unsound behavior. `collections.abc.Set`'s definition of `set.__or__` (3), in combination with 6, leads mypy to accept `|=` with differently typed sets on the left and right side of the operator. For runtime sets that do not implement `__ior__`, that works out great because it would return a new set. But for the builtin `set`, this results in the left-hand set being updated, and stepping out of its type constraints. stdlib's `set` tries to mitigate this through its signature for `__ior__`: it only allows its own type for the right hand side. But of course, the abstract base type does not see or have this signature, so mypy does not see it.
+Before we dive into the specifics of these six facts, let's look at how all this plays together to result in the unsound behavior. `collections.abc.Set`'s definition of `set.__or__` (3), in combination with 6, leads mypy to accept `|=` with differently typed sets on the left and right side of the operator. For runtime sets that do not implement `__ior__`, that works out great because it would return a new set. But for the builtin `set`, this results in the left-hand set being updated, and stepping out of its type constraints. stdlib's `set` tries to mitigate this through its signature for `__ior__`: it only allows its own type for the right hand side. But of course, the abstract base type does not see or have this signature (parameter type narrowing in a child type is a violation of the Liskov substitution principle), so mypy does not see it.
 
 So, let's deep a bit deeper into each of the above:
 1. Makes perfect sense, and is a broadly applied rule.
@@ -67,25 +67,19 @@ So, let's deep a bit deeper into each of the above:
 
 Ok, now that we've covered the bases, let's have a critical look at all this.
 
-I think we can safely conclude that 2 and 6 are tightly coupled. 2 is required because 6 exists. If we were to change 2 without changing 6, we'd end up with more unsoundness, and vice versa (as proven by the type ignore). 2 is not required by itself, because the two definitions only need to be consistent if `<OP>=` is supported *and* `__iOP__` is not implemented but refined by a subtype. That is the mode that 6 exposes. Without that mode, this scenario could never exist.
+I think we can safely conclude that 2 and 6 are tightly coupled. 2 is required because 6 exists. If we were to change 2 without changing 6, we'd end up with more unsoundness, and vice versa (as proven by the type ignore). 2 is not required on its own, because the two definitions only need to be consistent if `<OP>=` is supported *and* `__iOP__` is not implemented but refined by a subtype. That is the mode that 6 exposes. Without that mode, this scenario could never exist.
 
 Another conclusion I think we can safely draw is that a broad definition, as in 3, is *always* the appropriate definition for `__OP__`, both due to 1, and simply because if we're returning a new object, there's really no reason to be restrictive.
 
 A final conclusion is that a narrow definition for `__iOP__`, as in 4, is *always* the appropriate definition for `__iOP__` (the ones that extend the object. The others are already defined broadly, as is appropriate, see e.g. `set.__isub__`): you can't extend the object with something of a different type. I mean, you can at runtime, but it's not sound in a static type system.
 
-Huh, that's interesting. These are contradictory conclusions. We conclude that 3 and 4 are always appropriate. So that must mean that 2 is simply too restrictive: it doesn't allow *any* sound definition of a covariant base type that offers `__OP__`, with concrete types that expand it with `__iOP__`. It will have got to go. And we can only get rid of 2 if we also get rid of 6. If we do both, we end up with a system that allows both 3 and 4, without ignores, and without the unsoundness that this whole thread is about.
+That's interesting. These are contradictory conclusions. We conclude that 3 and 4 are always appropriate. So that must mean that 2 is simply too restrictive: it doesn't allow *any* sound definition of a covariant base type that offers `__OP__`, with concrete types that expand it with `__iOP__`. It will have got to go. And we can only get rid of 2 if we also get rid of 6. If we do both, we end up with a system that allows both 3 and 4, without ignores, and without the unsoundness that this whole thread is about.
 
 So, while the type ignores are indeed a problem, I also think they are very much required. The real problem stems from the fact that they will always be required. And therefore the type checkers' assumption in 6 simply can not hold. Or at least that it essentially prohibits abstract covariant types from defining any operators at all.
 
-# Solutions
-
-The obvious solution seems to be to update mypy (and pyright I'm assuming) to drop the assumption that if `__OP__` is defined, the type supports `<OP>=`. As a bonus, it would also ensure that supposedly immutable types may be mutated, because that could be implemented now even without type ignores.
-
-I should disclaim, what I propose would be a solution to *this* problem. I do not know for sure if it might create other problems elsewhere, and if those can be soundly solved. This is something that would need to be investigated and discussed further. But I think that the first step has to be that this premise is accepted and acknowledged: the way mypy handles this know is simply broken for covariant abstract types like `collections.abc.Set`. And not in some subjective way that could be addressed by a strict mode, but objectively, in all such cases.
-
-But for certain other types, we have to make sure that `<OP>+` keeps on working. e.g. consider `int`'s `+=`. We'll have to find a way to drop the broken assumption, without breaking types like `int` in the meantime. It is still unclear if such an approach exists, or what it would look like.
+However, in the current state of things, the unfortunate reality is that this unsoundness exists, and any potential solutions would require careful discussion to ensure that we do not break any other interfaces (e.g. primitives' `<OP>=`).
+This docstring is not the place for such discussion. A tentative proposal was made in the previously linked PR thread, but it will have to be taken to the Python discussion forum before any action can be taken.
 """
-# TODO: update description with analysis from PR thread
 
 from collections.abc import Set as AbstractSet
 
--- examples/stdlib/mutable_inplace_indirect.py
+++ examples/stdlib/mutable_inplace_indirect.py
@@ -1,14 +1,14 @@
 """
-This example is conceptually the same as mutable_inplace, but it adds another layer of indirection.
+This example is conceptually the same as mutable_inplace, but it adds another layer of indirection to expose another cause
+for the.same unsoundness.
 
-Pyright seems to have a feature where it ignores annotations in favor of the inferred type. This causes the same unsoundness
+Pyright seems to have a feature where it ignores annotations in favor of the (narrower) inferred type. This causes the same unsoundness
 as highlighted by mutable_inplace, but for a different root cause: the inferred type is built-in set rather than ABC set.
 See the comment on `other` in mutable_inplace.
 
 This example is therefore a more indirect reproduction of the same root cause as in mutable_inplace, working around the pyright
 behavior, to showcase that even below the incorrectly inferred type, it suffers from the same issue that mypy does.
 """
-# TODO: review @decorator-factory's comment on the PR thread to see if anything is missing from the description.
 
 from collections.abc import Set as AbstractSet
 
