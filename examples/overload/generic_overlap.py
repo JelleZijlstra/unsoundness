eb83cee1e8454be01e85c4eaea0967f9177131a9
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2026-05-06 09:36:39 -0700

    Add another overload example

---
+++ examples/overload/generic_overlap.py
@@ -0,0 +1,29 @@
+from typing import overload, Any, TypeVar
+
+T = TypeVar("T")
+
+
+@overload
+def f(x: list[T]) -> T: ...
+
+
+@overload
+def f(x: T) -> T: ...
+
+
+def f(x: Any) -> Any:
+    if isinstance(x, list):
+        return x[0]
+    return x
+
+
+def wrapper(x: T) -> T:
+    return f(x)
+
+
+def func(x: int, cond: bool = False) -> str:
+    res = wrapper([x]) if not cond else "hi"
+    if isinstance(res, list):
+        return str(res[0])
+    else:
+        return res
