5c1112a38ebf013a95edc592537ee3d23e5539f3
Jelle Zijlstra <jelle.zijlstra@gmail.com>
2026-05-09 19:58:00 -0700

    Add __eq__ override example (#50)

---
+++ examples/narrowing/eq_override.py
@@ -0,0 +1,31 @@
+"""Overriding __eq__ and __hash__ on a str subclass allows unsound narrowing.
+
+This one doesn't currently work in ty, because it does not do unsound narrowing on __eq__.
+"""
+
+from typing import TypedDict
+
+
+class TD(TypedDict):
+    a: int
+    b: str
+
+
+class Evil(str):
+    def __eq__(self, other: object) -> bool:
+        return True
+
+    def __hash__(self) -> int:
+        return hash("a")
+
+
+def get(td: TD, key: str) -> str:
+    match key:
+        case "b":
+            return td[key]
+    return "hi"
+
+
+def func(x: int) -> str:
+    td: TD = {"a": x, "b": str(x)}
+    return get(td, Evil("b"))
